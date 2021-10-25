a0ab0b4e7d3a1741bdb6d175df45664ac1ed7256
apurba <apurbakumar.3110@gmail.com>
2021-10-25 17:36:27 +0530

    Adding code to skeleton file

---
+++ src/main/java/com/eDonationSystem/EDonationSystemApplication.java
@@ -0,0 +1,14 @@
+package com.eDonationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDonationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EDonationSystemApplication.class, args);
+		System.out.println("Test is running!");
+	}
+
+}
